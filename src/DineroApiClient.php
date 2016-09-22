454da3f8bc26a26f2995625a5d86c9118ebfd89d
Peter Suhm <peter@suhm.dk>
2016-09-22 14:11:55 +0200

    post to the API ✉️🏆

--- src/DineroApiClient.php
+++ src/DineroApiClient.php
@@ -3,6 +3,7 @@
 namespace Dinero;
 
 use GuzzleHttp\Client;
+use Psr\Http\Message\ResponseInterface;
 
 class DineroApiClient
 {
@@ -10,6 +11,11 @@ class DineroApiClient
      * @const string
      */
     const AUTH_URL = 'https://authz.dinero.dk/dineroapi/oauth/token';
+
+    /**
+     * @const string
+     */
+    const BASE_URL = 'https://api.dinero.dk/v1/';
 
     /**
      * @var string
@@ -37,20 +43,30 @@ class DineroApiClient
     private $token;
 
     /**
+     * @var string
+     */
+    private $orgId;
+
+    /**
      * DineroApiClient constructor.
      * @param Client $httpClient
      * @param $clientId
      * @param $clientSecret
      * @param $apiKey
+     * @param $orgId
      */
-    public function __construct(Client $httpClient, $clientId, $clientSecret, $apiKey)
+    public function __construct(Client $httpClient, $clientId, $clientSecret, $apiKey, $orgId)
     {
         $this->httpClient = $httpClient;
         $this->clientId = $clientId;
         $this->clientSecret = $clientSecret;
         $this->apiKey = $apiKey;
+        $this->orgId = $orgId;
     }
 
+    /**
+     * Obtain a fresh access token for the Dinero API.
+     */
     public function refreshToken()
     {
         $response = json_decode($this->httpClient->post($this::AUTH_URL, [
@@ -66,21 +82,52 @@ class DineroApiClient
         $this->token = $response['access_token'];
     }
 
+    /**
+     * Post to the Dinero API.
+     *
+     * @param $uri
+     * @param array $body
+     * @return ResponseInterface
+     */
+    public function post($uri, array $body = [])
+    {
+        $url = $this::BASE_URL . $this->orgId . $uri;
+
+        return $this->httpClient->post($url, [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->token()
+            ],
+            'json' => $body,
+        ]);
+    }
+
+    /**
+     * @return string
+     */
     public function token()
     {
         return $this->token;
     }
 
+    /**
+     * @return string
+     */
     private function getClientId()
     {
         return $this->clientId;
     }
 
+    /**
+     * @return string
+     */
     private function getClientSecret()
     {
         return $this->clientSecret;
     }
 
+    /**
+     * @return string
+     */
     private function getApiKey()
     {
         return $this->apiKey;
